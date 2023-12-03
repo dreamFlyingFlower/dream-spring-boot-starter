e9d3b576aa6eb2c4b48085a1ff56b5cacff7d643
DreamFlyingFlower <58282232@qq.com>
2023-12-03 22:07:44 +0800

    study something

--- dream-spring-boot-starter-excel/src/main/java/dream/flying/flower/autoconfigure/excel/DreamExcelAutoConfiguration.java
+++ dream-spring-boot-starter-excel/src/main/java/dream/flying/flower/autoconfigure/excel/DreamExcelAutoConfiguration.java
@@ -3,12 +3,8 @@ package dream.flying.flower.autoconfigure.excel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 
 /**
- * 格式化LocalDateTime,不可继承 WebMvcConfigurationSupport,有可能会改变JSON序列化方式
- * 
- * 序列化方式需要借助原{@link JacksonProperties}配置
  *
  * @author 飞花梦影
  * @date 2022-12-05 17:28:05
